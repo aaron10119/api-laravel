587dc9b0d43f559fe49b3decbb3d1acfaedccaea
Aaron <aaronhernandezbueno9@gmail.com>
2025-03-14 00:57:43 -0600

    end points categoria crud

--- routes/api.php
+++ routes/api.php
@@ -2,28 +2,38 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\ProductController;
+
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get('/products', function(){
-    return 'Obteniendo Productos';
-});
 
-Route::get('/products{id}', function(){
-    return 'Obteniendo un producto';
-});
+//CATEGORIAS
+Route::get('/categories', [CategoryController::class, 'index']);
 
-Route::post('/products', function(){
-    return 'Creando Productos';
-});
+Route::get('/categories/{id}', [CategoryController::class, 'show']);
 
-Route::put('/products{id}', function(){
-    return 'Actualizando producto';
-});
+Route::post('/categories', [CategoryController::class, 'store']);
 
-Route::delete('/products{id}', function(){
-    return 'Eliminar Producto';
-});
+Route::put('/categories/{id}', [CategoryController::class, 'update']);
+
+Route::delete('/categories/{id}', [CategoryController::class, 'destroy']);
+
+
+
+
+//PRODUCTOS
+Route::get('/products', [ProductController::class, 'index']);
+
+Route::get('/products/{id}', [ProductController::class, 'show']);
+
+Route::post('/products', [ProductController::class, 'store']);
+
+Route::put('/products/{id}', [ProductController::class, 'update']);
+
+Route::delete('/products/{id}', [ProductController::class, 'destroy']);
+
 
